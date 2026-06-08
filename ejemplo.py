b448df11beca54a3a5307aaf61e129e718051a68
Rodrigo Fajardo <doresriddim@gmail.com>
2026-06-08 10:46:35 -0400

    Avance guía listas

--- ejemplo.py
+++ ejemplo.py
@@ -3,7 +3,7 @@
 #print(Resetea tu cerebro)
 #print(Cabeza hueca se te olvido setear el guardado automático)
 #RECUERDA ESTE ORDEN: 
-# 1.- MODIFICAR EL CÓDIGO   2.- GIT ADD.  3.- GIT COMMIT -M "COMENTARIO DENTRO"   4.- GIT PUSH PARA GUARDAR EN LÍNEA
+# 1.- MODIFICAR EL CÓDIGO   2.- GIT ADD .  3.- GIT COMMIT -M "COMENTARIO DENTRO"   4.- GIT PUSH PARA GUARDAR EN LÍNEA
 
 # 1. Control de asistencia
 # asistencia = ["Ana", "Carlos", "Elena", "Tomás"] 
@@ -22,10 +22,19 @@
 # b) Indique cuál es el producto con mayor stock y cuál con menor stock. 
 # c) Agregue un nuevo valor de stock ingresado por teclado.
 
-# stock = [15, 8, 23, 12, 5, 25]
+# 3. stock = [15, 8, 23, 12, 5, 25]
 # suma = sum(stock)
 # print(suma)
 # maximo = max(stock)
 # print(maximo)
 # minimo = min(stock)
 # print(minimo)
+
+# 3. Lista de espera de una consulta médica
+# pacientes = ["María", "Pedro", "Sofía", "Luis"] 
+# a) Muestre el primer paciente atendido (elimínelo de la lista). 
+# b) Agregue un paciente urgente en la primera posición. 
+# c) Muestre la lista actualizada.
+
+# pacientes = ["María", "Pedro", "Sofía", "Luis"]
+# print(pacientes[0])
