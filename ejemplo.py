903f80ee863f33b1b78aad0032bcf9fcae4feaf9
Rodrigo Fajardo <doresriddim@gmail.com>
2026-06-08 10:19:37 -0400

    Por fin avanzas en algo caracol

--- ejemplo.py
+++ ejemplo.py
@@ -1,6 +1,31 @@
 #Practicando github en línea
-print(Cabeza dura no entiendes)
-print(Resetea tu cerebro)
-print(Cabeza hueca se te olvido setear el guardado automático)
+# print(Cabeza dura no entiendes)
+#print(Resetea tu cerebro)
+#print(Cabeza hueca se te olvido setear el guardado automático)
 #RECUERDA ESTE ORDEN: 
 # 1.- MODIFICAR EL CÓDIGO   2.- GIT ADD.  3.- GIT COMMIT -M "COMENTARIO DENTRO"   4.- GIT PUSH PARA GUARDAR EN LÍNEA
+
+# 1. Control de asistencia
+# asistencia = ["Ana", "Carlos", "Elena", "Tomás"] 
+# a) Agregue un nuevo estudiante ingresado por teclado al final de la lista. 
+# b) Muestre cuántos estudiantes asistieron. 
+# c) Ordene alfabéticamente la lista y la muestre.
+
+# asistencia = ["Ana", "Carlos", "Elena", "Tomás", "Rodrigo"]
+# print(len(asistencia))
+# asistencia.sort()
+# print(asistencia)
+
+# 2. Inventario de una tienda
+# stock = [15, 8, 23, 12, 5] 
+# a) Muestre el stock total. 
+# b) Indique cuál es el producto con mayor stock y cuál con menor stock. 
+# c) Agregue un nuevo valor de stock ingresado por teclado.
+
+# stock = [15, 8, 23, 12, 5, 25]
+# suma = sum(stock)
+# print(suma)
+# maximo = max(stock)
+# print(maximo)
+# minimo = min(stock)
+# print(minimo)
