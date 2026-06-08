d5c9fb6610c708142882e11bbd7f2bbe96253b25
Rodrigo Fajardo <doresriddim@gmail.com>
2026-06-08 09:48:19 -0400

    Grabatelo de una vez cabeza hueca

--- ejemplo.py
+++ ejemplo.py
@@ -3,4 +3,4 @@ print(Cabeza dura no entiendes)
 print(Resetea tu cerebro)
 print(Cabeza hueca se te olvido setear el guardado automático)
 #RECUERDA ESTE ORDEN: 
-# 1.- MODIFICAR EL CÓDIGO   2.- GIT ADD.      3.- GIT COMMIT -M "COMENTARIO DENTRO"
+# 1.- MODIFICAR EL CÓDIGO   2.- GIT ADD.  3.- GIT COMMIT -M "COMENTARIO DENTRO"   4.- GIT PUSH PARA GUARDAR EN LÍNEA
